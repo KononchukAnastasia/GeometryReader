8a4163043455de7d0f9b01d0ab3d22ce37482da2
Anastasia Kononchuk <nastyakononchuk@me.com>
2024-04-13 20:45:53 +0700

    Initial Commit

---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Анастасия Конончук on 13.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
